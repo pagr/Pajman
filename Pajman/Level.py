467b2ff57add95b16caa414222e8879ae5ef14d5
Mackiovello <elandroe@icloud.com>
2016-11-18 20:12:36 +0100

    change to spaces

--- Pajman/Level.py
+++ Pajman/Level.py
@@ -1,10 +1,11 @@
 import pygame
 
 class Level:
-	def __init__(self, screen, width, height):
-		self.width = width
-		self.height = height
-		self.screen = screen
+    def __init__(self, screen, width, height):
+        self.width = width
+        self.height = height
+        self.screen = screen
 
-	def draw(self):
-		rect = pygame.draw.rect(self.screen, (255, 0, 255), (0, 0, self.width, self.height))
+    def draw(self):
+        rect = pygame.draw.rect(self.screen, (255, 255, 255), (0, 0, self.width, self.height))
+        outer_wall = pygame.draw.lines(self.screen, (255, 255, 255), [(10, 50), (200, 200)], 5)
--- Pajman/Pajman.py
+++ Pajman/Pajman.py
@@ -12,13 +12,13 @@ map = Level.Level(screen, 50, 50)
 pacman = Pacman.Pacman(screen)
 
 while(True):
-	screen.fill((0,0,0))
-	map.draw()
-	pacman.draw()
-	keys = pygame.key.get_pressed()
-	pacman.update(keys)
+    screen.fill((0,0,0))
+    map.draw()
+    pacman.draw()
+    keys = pygame.key.get_pressed()
+    pacman.update(keys)
 
-	pygame.display.flip()
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
